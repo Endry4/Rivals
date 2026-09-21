0c37ca0fc18e4641dfc0966ad3f087c80a44722d
Endry4 <55649506+Endry4@users.noreply.github.com>
2026-09-21 15:46:12 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,22 @@ export const botConfig = {
       {
         name: "Hood Rivals", // required by Discord API, not shown in the client
         state: "Hood Rivals Dicord",     // this is what people actually see
-        type: 1,               // Hood Rivals
+        type: 1,               //static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Playing Solo";
+    discordPresence.details = "Competitive";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "https://share.icloud.com/photos/0d5gWVIGLxU88ZFr4pFuPlCMQ";
+    discordPresence.smallImageText = "Rogue - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 1;
+    discordPresence.partyMax = 1;
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
       },
     ],
   },
