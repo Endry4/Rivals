b10207d47dc03bc88f70c75166807003b6a2faab
Endry4 <55649506+Endry4@users.noreply.github.com>
2026-09-21 16:35:43 -0700

    Update crime.js

--- src/commands/Economy/crime.js
+++ src/commands/Economy/crime.js
@@ -4,8 +4,8 @@ import { getEconomyData, setEconomyData } from '../../utils/economy.js';
 import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const CRIME_COOLDOWN = 0 * 10 * 1000;
-const JAIL_TIME = 2 * 60 * 60 * 1000;
+const CRIME_COOLDOWN = 0 * 1 * 1000;
+const JAIL_TIME = 0 * 0 * 0 * 1000;
 const FINE_RATE = 0.2;
 
 const CRIME_TYPES = [
