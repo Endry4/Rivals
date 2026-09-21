73363e1547e2c3075e334dbdac3e2b75781c3a8d
Endry4 <55649506+Endry4@users.noreply.github.com>
2026-09-21 07:16:33 -0700

    Update work.js

--- src/commands/Economy/work.js
+++ src/commands/Economy/work.js
@@ -6,9 +6,9 @@ import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 import { botConfig } from '../../config/bot.js';
 
-const WORK_COOLDOWN = botConfig.economy?.cooldowns?.work ?? 30 * 60 * 1000;
-const MIN_WORK_AMOUNT = botConfig.economy?.workMin ?? 10;
-const MAX_WORK_AMOUNT = botConfig.economy?.workMax ?? 100;
+const WORK_COOLDOWN = botConfig.economy?.cooldowns?.work ?? 0 * 10 * 1000;
+const MIN_WORK_AMOUNT = botConfig.economy?.workMin ?? 1000;
+const MAX_WORK_AMOUNT = botConfig.economy?.workMax ?? 10000;
 const LAPTOP_MULTIPLIER = 1.5;
 const WORK_JOBS = [
     "Software Developer",
